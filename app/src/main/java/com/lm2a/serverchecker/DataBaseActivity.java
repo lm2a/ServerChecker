306efe0fb207dd9d5988705b9ab8a5d6f4b09545
lemenzm <mario.lamenza@gmail.com>
2016-09-08 16:44:37 +0100

    All screen almost done. Should add a customer view at the dialog and improve design.

--- app/src/main/java/com/lm2a/serverchecker/DataBaseActivity.java
+++ app/src/main/java/com/lm2a/serverchecker/DataBaseActivity.java
@@ -1,13 +1,27 @@
 package com.lm2a.serverchecker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.lm2a.serverchecker.database.DatabaseHelper;
+import com.lm2a.serverchecker.database.HostsAdapter;
 import com.lm2a.serverchecker.model.Email;
 import com.lm2a.serverchecker.model.Host;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,14 +31,135 @@ public class DataBaseActivity extends Activity {
 
     // Database Helper
     DatabaseHelper db;
+    Host mHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.hosts);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        Button add = (Button) findViewById(R.id.btnAddHost);
+        final EditText url = (EditText) findViewById(R.id.url);
+        final EditText port = (EditText) findViewById(R.id.port);
+        final CheckBox notification = (CheckBox)findViewById(R.id.notification);
+        final CheckBox email = (CheckBox)findViewById(R.id.email);
+
+        final ImageButton addEmail = (ImageButton) findViewById(R.id.addButton);
+        final TextView label =  (TextView) findViewById(R.id.textView);
 
         db = new DatabaseHelper(getApplicationContext());
+        List<Host> hosts = db.getAllHosts();
 
+        addEmail.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                List<Email> emails = db.getEmailsByHost(mHost.getHost());
+                String[] stringArray = getArrayFromList(emails);
+/*              Custom View
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+// ...Irrelevant code for customizing the buttons and title
+                LayoutInflater inflater = this.getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.alert_label_editor, null);
+                dialogBuilder.setView(dialogView);
+
+                EditText editText = (EditText) dialogView.findViewById(R.id.label_field);
+                editText.setText("test label");
+                AlertDialog alertDialog = dialogBuilder.create();
+                alertDialog.show();*/
+
+
+                AlertDialog.Builder builderSingle = new AlertDialog.Builder(DataBaseActivity.this);
+                builderSingle.setIcon(R.drawable.cast_ic_notification_small_icon);
+                builderSingle.setTitle("Select One Name:-");
+
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                        DataBaseActivity.this,
+                        android.R.layout.select_dialog_singlechoice);
+                arrayAdapter.addAll(stringArray);
+//                arrayAdapter.add("Hardik");
+//                arrayAdapter.add("Archit");
+//                arrayAdapter.add("Jignesh");
+//                arrayAdapter.add("Umang");
+//                arrayAdapter.add("Gatti");
+                db.closeDB();
+                builderSingle.setNegativeButton(
+                        "cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                builderSingle.setAdapter(
+                        arrayAdapter,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String strName = arrayAdapter.getItem(which);
+                                AlertDialog.Builder builderInner = new AlertDialog.Builder(
+                                        DataBaseActivity.this);
+                                builderInner.setMessage(strName);
+                                builderInner.setTitle("Your Selected Item is");
+                                builderInner.setPositiveButton(
+                                        "Ok",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(
+                                                    DialogInterface dialog,
+                                                    int which) {
+                                                dialog.dismiss();
+                                            }
+                                        });
+                                builderInner.show();
+                            }
+                        });
+                builderSingle.show();            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //is chkIos checked?
+                if (((CheckBox) v).isChecked()) {
+                    addEmail.setVisibility(View.VISIBLE);
+                    label.setVisibility(View.VISIBLE);
+                } else {
+                    addEmail.setVisibility(View.INVISIBLE);
+                    label.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+
+
+// Create the adapter to convert the array to views
+        HostsAdapter adapter = new HostsAdapter(this, (ArrayList<Host>) hosts);
+
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position,
+                                    long arg3)
+            {
+                mHost = (Host)adapter.getItemAtPosition(position);
+                url.setText(mHost.getHost());
+                port.setText(mHost.getPort());
+                notification.setChecked(mHost.isNotification());
+                email.setChecked(mHost.isEmails());
+                // assuming string and if you want to get the value on click of list item
+                // do what you intend to do on click of listview row
+            }
+        });
 /*
         Email tag1 = new Email("bebay1@krebay.com");
         Email tag2 = new Email("bebay2@krebay.com");
@@ -65,10 +200,21 @@ public class DataBaseActivity extends Activity {
         db.createHostEmail(todo13_id, tag5_id);
         db.createHostEmail(todo13_id, tag8_id);*/
 
-        List<Email> emails = db.getEmailsByHost("www.lm2a3.com");
-        Log.i("Bebay: ", emails.toString());
-        // Don't forget to close database connection
-        db.closeDB();
+//        List<Email> emails = db.getEmailsByHost("www.lm2a3.com");
+//        Log.i("Bebay: ", emails.toString());
+//        // Don't forget to close database connection
+//        db.closeDB();
 
+    }
+
+    public String[] getArrayFromList(List<Email> emails){
+        String[] a = new String[emails.size()];
+
+        for (int i = 0; i < emails.size(); i++) {
+            Email e = emails.get(i);
+            a[i] = e.getEmail();
+        }
+
+        return a;
     }
 }
---
+++ app/src/main/java/com/lm2a/serverchecker/database/HostsAdapter.java
@@ -0,0 +1,37 @@
+package com.lm2a.serverchecker.database;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.lm2a.serverchecker.R;
+import com.lm2a.serverchecker.model.Host;
+
+import java.util.ArrayList;
+
+public class HostsAdapter extends ArrayAdapter<Host> {
+    public HostsAdapter(Context context, ArrayList<Host> hosts) {
+       super(context, 0, hosts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       // Get the data item for this position
+       Host host = getItem(position);
+       // Check if an existing view is being reused, otherwise inflate the view
+       if (convertView == null) {
+          convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_host, parent, false);
+       }
+       // Lookup view for data population
+       TextView tvHost = (TextView) convertView.findViewById(R.id.tvHost);
+       TextView tvPrt = (TextView) convertView.findViewById(R.id.tvPort);
+       // Populate the data into the template view using the data object
+        tvHost.setText(host.getHost());
+        tvPrt.setText(host.getPort());
+       // Return the completed view to render on screen
+       return convertView;
+   }
+}
